b955d7f8d43a4b14dd6e5ba9b3733cc3c8e8deea
natalisuportman <evangelos.ilias87@gmail.com>
2023-06-25 18:51:14 +0300

    Updated User model and RouteServiceProvider

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Demo\DemoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,12 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Creting Route controller and group them
-Route::controller(DemoController::class)->group(function () {
-    Route::get('/about', 'Index')->middleware('check');
-    Route::get('/contact', 'Contact');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
-
-
+require __DIR__.'/auth.php';
